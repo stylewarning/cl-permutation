65fc1707e9f01e3189c2a68245ba1a083b138df7
Robert Smith <quad@symbo1ics.com>
2012-05-06 20:56:05 -0500

    * add PERM-REF for referencing elements of the perm spec * add PERM-SIZE to compute the size of the permutation * add PERM-LENGTH to calculate the number of inversions * add PERM-{EVEN, ODD}-P to check if the perm is even or odd * add PERM-COMPOSE for permutation composition * add PERM-INVERSE to compute the inverse of a perm

--- permutation.lisp
+++ permutation.lisp
@@ -56,3 +56,64 @@
   ;; Set ]
   (set-macro-character #\] (get-macro-character #\))))
 
+
+;;;;;;;;;;;;;;;;;;;;;;; Permutation operations ;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun perm-ref (perm n)
+  "Compute what N maps to in the permutation PERM."
+  (assert (<= 0 n (1- (perm-size perm)))
+          (n)
+          "Permutation index of ~D must be within the length of the ~
+           permutation ~A."
+          n perm)
+  (aref (perm.spec perm) n))
+
+(defun perm-size (perm)
+  "The size of a permutation PERM."
+  (length (perm.spec perm)))
+
+(defun perm-length (perm)
+  "Count the number of inversions in the permutation PERM."
+  (let ((spec      (perm.spec perm))
+        (n         (perm-size perm))
+        (inv-count 0))
+    (loop :for i :below (1- n)
+          :do (loop :for j :from (1+ i) :to (1- n)
+                    :when (> (svref spec i)
+                             (svref spec j))
+                    :do (incf inv-count)))
+    
+    inv-count))
+
+(defun perm-even-p (perm)
+  "Is PERM an even permutation?"
+  (evenp (perm-length perm)))
+
+(defun perm-odd-p (perm)
+  "Is PERM an odd permutation?"
+  (oddp (perm-length perm)))
+
+(defun perm-compose (p1 p2)
+  "Compose the permutations P1 and P2."
+  (assert (= (perm-size p1)
+             (perm-size p2))
+          nil
+          "Permutations ~A and ~A must have the same size."
+          p1 p2)
+  
+  (let* ((n        (perm-size p1))
+         (p12-spec (make-array n :element-type '(unsigned-byte *)
+                                 :initial-element 0)))
+    (loop :for i :below n
+          :do (setf (aref p12-spec i)
+                    (perm-ref p1 (perm-ref p2 i)))
+          :finally (return (make-perm :spec p12-spec)))))
+
+(defun perm-inverse (perm)
+  "Find the inverse of the permutation PERM."
+  (let* ((n          (perm-size perm))
+         (perm*-spec (make-array n :element-type '(unsigned-byte *)
+                                   :initial-element 0)))
+    (loop :for i :below n
+          :do (setf (aref perm*-spec (perm-ref perm i)) i)
+          :finally (return (make-perm :spec perm*-spec)))))
