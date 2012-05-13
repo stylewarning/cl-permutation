a9bd35980e11b4cdc15a1a368c6b2a3c034d3f94
Robert Smith <quad@symbo1ics.com>
2012-05-13 02:28:08 -0500

    A bunch of fixes for permutation group calculations. Works!

--- permutation-group.lisp
+++ permutation-group.lisp
@@ -3,36 +3,72 @@
 
 (in-package #:cl-permutation)
 
-(defun group-element-p (perm group)
-  (labels ((is-in (perm k)
-             (when (zerop k)
-               (setf k (perm-size p)))
-             
-             (or (= 1 k)
-                 (let ((j (perm-eval p k)))
-                   (multiple-value-bind (k-val k-exists-p) (gethash k group)
-                     (when k-exists-p
-                       (multiple-value-bind (j-val j-exists-p) (gethash j k-val)
-                         (when j-exists-p
-                           (is-in (perm-compose j-val p) (1- k))))))))))
-    (is-in perm group 0)))
+(defvar *prods*)
 
-(defun add-generator (perm trans group &optional (k 0))
-  ;; ...
-  )
+(defun sigma (group k j)
+  (safe-gethash j (safe-gethash k group)))
 
-(defun update-transversal (perm trans group &optional (k 0))
-  ;; ...
-  )
+(defun group-element-p (perm group &optional (k (perm-size perm)))
+  (or (= 1 k)
+      (let ((j (perm-eval perm k)))
+        (multiple-value-bind (k-val k-exists-p) (gethash k group)
+          (when k-exists-p
+            (multiple-value-bind (j-val j-exists-p) (gethash j k-val)
+              (when j-exists-p
+                (group-element-p (perm-compose (perm-inverse j-val) perm) 
+                                 group 
+                                 (1- k)))))))))
+
+(defun add-generator (perm trans group &optional (k (perm-size perm)))
+  (setf (gethash k trans)
+        (union (gethash k trans) (list perm)))
+  
+  (block :outer
+    (let ((redo nil))
+      (loop
+        (setf redo nil)
+        
+        (loop :for s :in (hash-table-values (gethash k group))
+              :do (loop :for tt :in (gethash k trans)
+                        :for prod := (perm-compose tt s)
+                        :do (progn
+                              (setf (gethash prod *prods*) t)
+                              
+                              (when (and (hash-table-key-exists-p *prods* prod)
+                                         (not (group-element-p prod group)))
+                                (multiple-value-setq (trans group)
+                                  (update-transversal prod trans group k))
+                                
+                                (setf redo t)))))
+        
+        (unless redo
+          (return-from :outer)))))
+
+  (values trans group))
+
+(defun update-transversal (perm trans group &optional (k (perm-size perm)))
+  (let ((j (perm-eval perm k)))
+    (handler-case
+        (let ((new-perm (perm-compose (perm-inverse (sigma group k j))
+                                      perm)))
+          (if (group-element-p new-perm group (1- k))
+              (values trans group)
+              (add-generator new-perm trans group (1- k))))
+      (hash-table-access-error (c) 
+        (declare (ignore c))
+        (progn
+          (setf (gethash j (gethash k group)) perm)
+          (values trans group))))))
 
 (defun strong-generating-set (generators)
   (labels ((identity-table (n)
-             "Make a hash table mapping I to the identity permutation
-of size I for 1 <= I <= N."
+             "Make a hash table mapping N to the identity permutation
+of size N"
              (let ((ht (make-hash-table)))
-               (dotimes (i n ht)
-                 (setf (gethash (1+ i) ht)
-                       (perm-identity (1+ i)))))))
+               (setf (gethash n ht)
+                     (perm-identity n))
+               
+               ht)))
 
     (let ((n (maximum generators :key 'perm-size))
           (trans (make-hash-table))
@@ -42,6 +78,9 @@ of size I for 1 <= I <= N."
       (dotimes (i n)
         (setf (gethash (1+ i) group) (identity-table (1+ i))))
       
+      ;; Initialize the product table
+      (setf *prods* (make-hash-table))
+      
       ;; Add the generators.
       (loop :for generator :in generators
             :do (multiple-value-setq (trans group)
@@ -50,4 +89,5 @@ of size I for 1 <= I <= N."
 
 (defun group-order (generators)
   (let ((transversals (strong-generating-set generators)))
-    (product (hash-table-values transversals) :key 'hash-table-size)))
+    (product (hash-table-values transversals) :key 'hash-table-count)))
+
