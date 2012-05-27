e620fba8bfeceeafb6931fcff235033893215346
Robert Smith <quad@symbo1ics.com>
2012-05-27 14:24:43 -0500

    Add exports to package definition (for permutation.lisp and permutation-group.lisp).

--- package.lisp
+++ package.lisp
@@ -2,5 +2,33 @@
 ;;;; Copyright (c) 2012 Robert Smith
 
 (defpackage #:cl-permutation
-  (:use #:cl))
+  (:use #:cl)
+  (:export
+   ;; permutation.lisp
+   #:perm
+   #:enable-perm-reader
+   #:list-to-perm
+   #:perm-identity
+   #:perm-identity-p
+   #:random-perm
+   #:perm-ref
+   #:perm-eval
+   #:perm-eval*
+   #:perm-size
+   #:perm-length
+   #:perm-even-p
+   #:perm-odd-p
+   #:perm-compose
+   #:perm-expt
+   #:perm-order
+   #:perm-transpose-indexes
+   #:perm-transpose-entries
+   #:perm-inverse
+   
+   ;; permutation-group.lisp
+   #:perm-group
+   #:generate-perm-group
+   #:group-order
+   #:group-element-p
+   #:random-group-element))
 
