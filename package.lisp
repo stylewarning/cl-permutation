1cb85e61d8dcc0a027c7eca9b2694a0eaf241f66
Robert Smith <quad@symbo1ics.com>
2012-06-04 07:52:10 -0500

    * Add package nickname PERM. * Add cycle functions to export list.

--- package.lisp
+++ package.lisp
@@ -3,6 +3,7 @@
 
 (defpackage #:cl-permutation
   (:use #:cl)
+  (:nicknames #:perm)
   (:export
    ;; permutation.lisp
    #:perm
@@ -18,12 +19,22 @@
    #:perm-length
    #:perm-even-p
    #:perm-odd-p
+   #:perm-sign
    #:perm-compose
    #:perm-expt
    #:perm-order
    #:perm-transpose-indexes
    #:perm-transpose-entries
    #:perm-inverse
+   
+   #:orbit-of
+   #:rotate-cycle-clockwise             ; Possibly will be removed.
+   #:rotate-cycle-counterclockwise      ; Possibly will be removed.
+   #:normalize-cycle-order              ; Possibly will be removed.
+   #:normalize-cycles
+   #:to-cycles
+   #:from-cycles
+   #:cycles-to-one-line                 ; Possibly will be removed.
    
    ;; permutation-group.lisp
    #:perm-group
