6aa1b7ba724f95919297c5e566a8e73ed2aa5981
Robert Smith <quad@symbo1ics.com>
2015-10-31 20:56:12 -0700

    Rewrite DO-GROUP-ELEMENTS to use MIXED-RADIX-SPEC

--- src/do-group-elements.lisp
+++ src/do-group-elements.lisp
@@ -5,64 +5,38 @@
 (in-package #:cl-permutation)
 
 ;;;; Here we define a macro to iterate over all elements of a group in
-;;;; some order.
-;;;;
-;;;; XXX FIXME: Everything in this file is extremely inefficient and
-;;;; could be nearly trivially sped up.
+;;;; some order. See reference [2] for details.
 
-;;; Taken from https://bitbucket.org/tarballs_are_good/lisp-random/src/37a571c75755ae8ccad79d46bd0c0e9ef77e72da/miscellaneous_exercises/password-variants.lisp?at=default#cl-35
-(defun incrementer (signature)
-  (labels ((propagate (radix sig carry accum)
-             (cond
-               ((or (null radix)
-                    (null sig))
-                (if (plusp carry)
-                    (error "Radix overflow.")
-                    (nreverse accum)))
-               
-               ;; No overflow...
-               ((< (+ carry (car radix)) (car sig))
-                (propagate (cdr radix)
-                           (cdr sig)
-                           0
-                           (cons (+ carry (car radix))
-                                 accum)))
-               
-               ;; Overflow... Propagate the carries forward.
-               (t
-                (propagate (cdr radix)
-                           (cdr sig)
-                           1
-                           (cons 0 accum))))))
-    (let ((num (make-list (length signature) :initial-element 0)))
-      (lambda ()
-        (prog1 num
-          (setf num (ignore-errors (propagate num signature 1 nil))))))))
-
-;;; Defined in reference [2].
 (defun group-radix (group)
-  (map 'list #'hash-table-count (perm-group.transversal-system group)))
+  "Compute the radix of the group GROUP."
+  (map 'simple-vector #'hash-table-count (perm-group.transversal-system group)))
 
+;;; TODO: Make this cons less.
 (defun group-element-from-signature (group signature)
-  (let* ((perms (mapcar (lambda (trans n)
-                          (nth-value 1 (hash-table-elt trans n)))
-                        (coerce (perm-group.transversal-system group) 'list)
-                        signature))
-         (id (perm-identity (maximum perms :key #'perm-size))))
+  (let ((perms (loop :for trans :across (perm-group.transversal-system group)
+                     :for n :across signature
+                     :collect (nth-value 1 (hash-table-elt trans n)))))
     (reduce #'perm-compose
-            (mapcar (lambda (s)
-                      (perm-compose id s))
-                    perms))))
+            perms
+            :key (let ((id (perm-identity (maximum perms :key #'perm-size))))
+                   (lambda (s)
+                     (perm-compose id s))))))
 
 (defmacro do-group-elements ((var group &optional return) &body body)
   "Iterate through all of the elements of the group GROUP, binding each element to VAR and executing BODY. Optionally return a value specified by RETURN."
-  (let ((ggroup      (gensym "GROUP-"))
-        (incrementer (gensym "INCREMENTER-"))
-        (signature   (gensym "SIGNATURE-")))
+  (let ((ggroup    (gensym "GROUP-"))
+        (spec      (gensym "SPEC-"))
+        (signature (gensym "SIGNATURE-"))
+        (rank      (gensym "RANK-")))
     `(let* ((,ggroup ,group)
-            (,incrementer (incrementer (group-radix ,ggroup))))
-       (loop :for ,signature := (funcall ,incrementer) :then (funcall ,incrementer)
-             :while ,signature
-             :do (let ((,var (group-element-from-signature ,ggroup ,signature)))
-                   ,@body)
-             :finally (return ,return)))))
+            (,spec (make-mixed-radix-spec (group-radix ,ggroup))))
+       ;; Map across all elements of the group.
+       (map-spec
+        (lambda (,rank ,signature)
+          (declare (ignore ,rank))
+          (let ((,var (group-element-from-signature ,ggroup ,signature)))
+            (tagbody
+               ,@body)))
+        ,spec)
+       ;; Return the return value.
+       ,return)))
